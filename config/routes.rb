87335bba82ad4b733d9f4c8b19e631a8d08b43d1
sorefull <olegh.cherednichenko@gmail.com>
2017-01-04 11:24:38 +0200

    back

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :new, :create]
+  root :to => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
